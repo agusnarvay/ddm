e900fc75b11f3c6f42adcfc07b456f80889c4535
agusnarvay <anarvay93@gmail.com>
2025-04-27 16:01:22 +0400

    final final

--- javascript/funcion-inicio.js
+++ javascript/funcion-inicio.js
@@ -1,12 +1,11 @@
 // Definí una constante junto con un array, con una lista de colores para aplicar al título de forma dinámica, que permitirá cambiar aleatoriamente el color cada vez que cargue la página - elegí colores que 'peguen' con la estética del proyecto
-const colores = ["#005DA1", "#089565", "#458bd1", "#ffffff"];
+const colores = ["#005DA1", "#6684A3", "#CA0B0B", "#ffffff"];
 //con esta constante 'random' - Math.floor y Math.random se van a elegir 'colores' al azar - donde se obtiene un número válido dentro del array y elige uno de los colores proporcionados.
 const random = colores[Math.floor(Math.random() * colores.length)];
 
 //Para hacer esto, primero tiene que estar listo el html y el css de los elementos que voy a manipular con js / 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("portada-titulo").style.color = random; //Aquí estoy accediendo al h1 de la portada, que su id es 'portada-titulo', con el getElementById. Se modificará su color utilizando el color aleatorio que seleccioné anteriormente. Le da cierto dinamismo y puede ser visualmente entretenido para el usuario
-});
+document.getElementById("portada-titulo").style.color = random; //Aquí estoy accediendo al h1 de la portada, que su id es 'portada-titulo', con el getElementById. Se modificará su color utilizando el color aleatorio que seleccioné anteriormente. Le da cierto dinamismo y puede ser visualmente entretenido para el usuario
+
 //noté que al hacer scroll en el menú desplegable, se hacía scroll a la nada porque el menú es corto y éste recorría todo el alto de pantalla. Asi que he utilizado este codigo de js para esconder el 'overflow' y que permita no hacer scroll a la pantalla. 
 document.querySelector(".abrir").addEventListener("click", () => {
     document.body.style.overflow = "hidden";
